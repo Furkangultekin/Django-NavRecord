6dba3878875a021d562c86785ea10b1285bbc90d
Furkangultekin <48828422+Furkangultekin@users.noreply.github.com>
2022-05-20 02:18:06 +0300

    Update urls.py

--- vehicle/urls.py
+++ vehicle/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .views import VehicleView, UpdateVehicleView, AddNavRecordView, LastPointsView
 
 urlpatterns = [
-    path('add_vehicle', VehicleView.as_view()),  # adding an item url; /add
+    path('add_vehicle', VehicleView.as_view()),
     path('get_vehicle', VehicleView.as_view()),
     path('update_vehicle', UpdateVehicleView.as_view()),
     path('add_nav_record', AddNavRecordView.as_view()),
